11de8be8f7031d0dc4ab169d12df1a7632b6d3d4
Masputrawae <masputrawae.official@gmail.com>
2025-06-23 17:06:52 +0700

    Changed On 📅 : 2025-06-23 17:06:52

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,14 @@ import { brainDbAstro, generateSlug } from '@braindb/astro'
 import tailwindcss from '@tailwindcss/vite'
 import icon from 'astro-icon'
 import mdx from '@astrojs/mdx'
-import pagefind from "astro-pagefind";
+import pagefind from 'astro-pagefind'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://masputrawae.github.io',
-  build: {
-    format: "file",
-  },
+	build: {
+		format: 'file'
+	},
 	image: {
 		domains: ['astro.build'],
 		remotePatterns: [{ protocol: 'https' }]
@@ -34,6 +34,6 @@ export default defineConfig({
 		}),
 		icon(),
 		mdx(),
-    pagefind()
+		pagefind()
 	]
 })
--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,8 +1,8 @@
-export function slugify(text:string) {
-  return text
-    .toLowerCase()                   // ubah jadi huruf kecil
-    .trim()                          // hilangkan spasi di awal/akhir
-    .replace(/[\s_]+/g, '-')         // ganti spasi dan _ jadi -
-    .replace(/[^\w\-]+/g, '')        // hilangkan karakter non-alfanumerik (opsional)
-    .replace(/\-\-+/g, '-');         // ganti -- jadi -
+export function slugify(text: string) {
+	return text
+		.toLowerCase() // ubah jadi huruf kecil
+		.trim() // hilangkan spasi di awal/akhir
+		.replace(/[\s_]+/g, '-') // ganti spasi dan _ jadi -
+		.replace(/[^\w\-]+/g, '') // hilangkan karakter non-alfanumerik (opsional)
+		.replace(/\-\-+/g, '-') // ganti -- jadi -
 }
