351759f21e937018a6de91c16fdb269b1b5c38e8
Masputrawae <masputrawae.official@gmail.com>
2025-07-11 16:12:19 +0700

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+import { } from './offcanvas.js';
+import { } from './collapse.js';
+import { } from './modal.js';
+import { } from './switchThemes.js';
+
+function main(){
+
+}
+
+document.addEventListener('DOMContentLoaded', main)
