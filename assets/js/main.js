d22fbd2ffc05a9fee493c41a195e9beb96d21690
Masputrawae <masputrawae.official@gmail.com>
2025-07-15 23:25:12 +0700

    Changed On 📅 : 2025-07-15 23:25:12

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,111 @@
-const draggableElements = document.querySelectorAll('.menu')
+const elHtml = document.documentElement
+const overlay = document.querySelector('.overlay')
 
-let currentElement = null
-let offsetX = 0
-let offsetY = 0
+// SWITCH THEME HANDLER
+function themeHandler() {
+  const KEY_THEME = 'THEME'
+  const button = document.querySelector('#switchTheme')
 
-draggableElements.forEach(el => {
-  el.addEventListener('mousedown', e => {
-    currentElement = el
-    offsetX = e.clientX - el.offsetLeft
-    offsetY = e.clientY - el.offsetTop
-    el.style.cursor = 'grabbing'
-  })
-})
-
-document.addEventListener('mousemove', e => {
-  if (!currentElement) return
-  currentElement.style.left = e.clientX - offsetX + 'px'
-  currentElement.style.top = e.clientY - offsetY + 'px'
-})
-
-document.addEventListener('mouseup', () => {
-  if (currentElement) {
-    currentElement.style.cursor = 'grab'
+  function setTheme(theme) {
+    elHtml.dataset.theme = theme
+    sessionStorage.setItem(KEY_THEME, theme)
+    if (theme === 'dark') {
+      button.classList.add('darkIsActive')
+    } else {
+      button.classList.remove('darkIsActive')
+    }
   }
-  currentElement = null
-})
 
-document.querySelectorAll('[data-menu]').forEach(menu => {
-  const targetId = menu.getAttribute('data-menu')
-  const targetEl = document.getElementById(targetId)
+  function initTheme() {
+    const isDarkSystem = window.matchMedia('(prefers-color-scheme: dark)')
+      .matches
+      ? 'dark'
+      : 'light'
 
-  menu.addEventListener('click', () => {
-    targetEl.classList.toggle('menu--isOpen')
+    const storageTheme = sessionStorage.getItem(KEY_THEME)
+    setTheme(storageTheme ? storageTheme : isDarkSystem)
+  }
+
+  function toggleTheme() {
+    button.addEventListener('click', () => {
+      const storageTheme = sessionStorage.getItem(KEY_THEME)
+      const newTheme = storageTheme === 'dark' ? 'light' : 'dark'
+      setTheme(newTheme)
+    })
+  }
+
+  initTheme()
+  toggleTheme()
+}
+
+// SIDEBAR HANDLER
+function sidebarHandler() {
+  const buttons = document.querySelectorAll('[data-sidebar]')
+
+  buttons.forEach(btn => {
+    const targetId = btn.getAttribute('data-sidebar')
+    const targetEl = document.getElementById(targetId)
+
+    btn.addEventListener('click', () => {
+      targetEl.classList.toggle('sidebar--isOpen')
+      overlay.classList.toggle('overlay--isActive')
+    })
+
+    overlay.addEventListener('click', () => {
+      targetEl.classList.remove('sidebar--isOpen')
+      overlay.classList.remove('overlay--isActive')
+    })
   })
-})
+}
+
+// TYPING
+function typingHandler() {
+  function startTypingEffect(
+    element,
+    texts,
+    typingSpeed = 150,
+    deletingSpeed = 50,
+    delay = 1000
+  ) {
+    let textIndex = 0
+    let charIndex = 0
+    let isDeleting = false
+
+    function type() {
+      const currentText = texts[textIndex]
+
+      if (!isDeleting) {
+        element.textContent = currentText.substring(0, charIndex + 1)
+        charIndex++
+
+        if (charIndex === currentText.length) {
+          isDeleting = true
+          setTimeout(type, delay)
+          return
+        }
+      } else {
+        element.textContent = currentText.substring(0, charIndex - 1)
+        charIndex--
+        if (charIndex === 1) {
+          isDeleting = false
+          textIndex = (textIndex + 1) % texts.length // next text
+        }
+      }
+      setTimeout(type, isDeleting ? deletingSpeed : typingSpeed)
+    }
+    type()
+  }
+
+  document.querySelectorAll('.typewriter').forEach(el => {
+    const texts = JSON.parse(el.getAttribute('data-texts'))
+    startTypingEffect(el, texts)
+  })
+}
+
+function main() {
+  themeHandler()
+  sidebarHandler()
+  typingHandler()
+}
+
+document.addEventListener('DOMContentLoaded', main)
